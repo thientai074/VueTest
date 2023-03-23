05efde2f2b4673ee928134cd325a1455975e9908
HungHv <hunghv@inet.vn>
2023-03-23 16:42:43 +0700

    First

---
+++ src/tests/HelloWorld.test.js
@@ -0,0 +1,13 @@
+import {mount} from "@vue/test-utils";
+import HelloWorld from "../components/HelloWorld.vue";
+// import { expect, test } from "vitest";
+test("mount component",async()=>{
+    expect(HelloWorld).toBeTruthy();
+
+    const wrapper = mount(HelloWorld,{
+        props:{
+            title:"Guess User Age App",
+        },
+    });
+    expect(wrapper.text()).toContain("Guess User Age App");
+})
